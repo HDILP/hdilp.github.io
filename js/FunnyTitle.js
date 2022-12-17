af87bcae115333fc9dcabefe9d8d995c6b72bd68
HDILP <awm31415@163.com>
2022-12-17 17:06:55 +0800

    Site updated: 2022-12-17 17:06:55

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,10 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/funny.ico");
         document.title = '╭(°A°`)╮ 诶 人呢 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = '(ฅ>ω<*ฅ) 你终于回来啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;

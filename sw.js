9b9ef2ad0a7fcddffc0f46bc87146de592f6ed67
HDILP <awm31415@163.com>
2023-01-03 10:33:43 +0800

    Site updated: 2023-01-03 10:33:42

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","ba64b45f01e72ca64ee7ce4df7589123"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
